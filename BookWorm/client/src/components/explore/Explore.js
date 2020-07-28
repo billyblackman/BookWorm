bc55a40e4dcf796d659e4fcb1b50577292c1bc9e
Billy Blackman <blackmanbilly@gmail.com>
2020-07-28 13:20:32 -0500

    Added google book component and list

--- BookWorm/client/src/components/explore/Explore.js
+++ BookWorm/client/src/components/explore/Explore.js
@@ -1,20 +1,59 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { GoogleBookContext } from "../../providers/GoogleBookProvider";
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { Form, FormGroup, Label, Media, Button, Card, CardImg, CardTitle, CardSubtitle, CardBody } from "reactstrap";
+import "../../styles/book.css"
 
 export default function Explore() {
 
     const { googleBooks, searchByTitle } = useContext(GoogleBookContext);
+
+    const [searched, setSearched] = useState(false);
+    const setSearchedState =() => setSearched(true);
+
+    const title = useRef();
+
+    const search = () => {
+        searchByTitle(title.current.value);
+        setSearchedState();
+    }
+
+    const bookItems = googleBooks.items;
+    // const bookItemsLength = bookItems.length;
+
+    const conditionalBookRender = () => {
+        return bookItems !== undefined ? (
+            <>
+            <div className="bookDiv">
+                {bookItems.map((book) => {
+                    return (
+                        <Card className="googleBook">
+                            <CardImg src={book.volumeInfo.imageLinks.thumbnail} />
+                            <CardBody>
+                                <CardTitle>{book.volumeInfo.title}</CardTitle>
+                                <CardSubtitle>{book.volumeInfo.subtitle}</CardSubtitle>
+                            </CardBody>
+                        </Card>
+                )
+            })}
+            </div>
+            </>
+        ) : (
+            <></>
+            )
+            
+    }
+    
 
     return (
         <>
             <Form>
                 <FormGroup>
                     <Label>Search by Title</Label>
-                    <Input type="text"></Input>
+                    <input type="text" name="title" ref={title}></input>
                 </FormGroup>
-                <Button>Search</Button>
+                <Button onClick={search}>Search</Button>
             </Form>
+            {conditionalBookRender()}
         </>
     );
 }
--- BookWorm/client/src/providers/GoogleBookProvider.js
+++ BookWorm/client/src/providers/GoogleBookProvider.js
@@ -4,11 +4,13 @@ export const GoogleBookContext = createContext();
 
 export const GoogleBookProvider = (props) => {
     const apiUrl = "https://www.googleapis.com/books/v1/volumes?q="
+    const apiKey = "AIzaSyBNMegrLmhBQj2J5ASYQxUEKKGYRmtyMhg"
 
     const [googleBooks, setGoogleBooks] = useState([]);
 
     const searchByTitle = (searchTerms) => {
         return fetch(`${apiUrl}${searchTerms}`)
+            .then((response) => response.json())
             .then(setGoogleBooks)
             .then(console.log(googleBooks));
 
